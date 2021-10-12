7c7b780f54883d33eb0fc604241a98856e11433e
Mustafa KARACABEY <mustafa.karacabey.engineer@gmail.com>
2021-10-12 21:59:00 +0300

    [Added-nullable-function]

--- src/functions/1-function.kt
+++ src/functions/1-function.kt
@@ -13,9 +13,15 @@ fun sumTwoNumbers(number1 : Int, number2 : Int) : Int {
     return number1 + number2
 }
 
+fun nullableFunction(number1 : Int, number2: Int) : Int? { //Nulable
+    return null
+}
+
 fun main() {
     showName()
     showParameter("KARACABEY")
+
+    println(nullableFunction(5, 8))
 
     var sum = sumTwoNumbers(1, 2)
     println(sum)
